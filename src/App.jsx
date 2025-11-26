e0cf6414949ef1dbbd11eab06d9a99e9305c87bb
coffee2023 <susig1212@gmail.com>
2025-11-25 22:15:51 -0500

    week 2 component task

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import './App.css';
 import React from 'react';
+import TodoList from './TodoList';
+import TodoForm from './TodoForm';
 
 function App() {
-  const todos = [
-    { id: 1, title: 'review resources' },
-    { id: 2, title: 'take notes' },
-    { id: 3, title: 'code out app' },
-  ];
+ 
   return (
     <div>
-      <h1>My Todos</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.title}</li>
-        ))}
-      </ul>
+      <h1>My Todos List</h1>
+      <TodoForm />
+      <TodoList />
+      
     </div>
   );
 }
